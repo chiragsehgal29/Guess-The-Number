2f4b3766f992f81626f9a3683e054d6a56ee8c87
Chirag Sehgal <74814189+chiragsehgal29@users.noreply.github.com>
2024-05-15 14:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const startOver = document.querySelector('.resultParas');
 const p = document.createElement('p');
 
 let prevGuess = [];
-let numGuess = 0;
+let numGuess = 1;
 
 let playGame = true;
 
@@ -58,7 +58,7 @@ function displayGuess(guess) {
   userInput.value = '';
   guessSlot.innerHTML += `${guess}, `;
   numGuess++;
-  remaining.innerHTML = `${10 - numGuess} `;
+  remaining.innerHTML = `${11 - numGuess} `;
 }
 
 function displayMessage(message) {
